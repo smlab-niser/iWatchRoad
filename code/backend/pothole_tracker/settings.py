b9b2df5d393408d300df60ab7d83e16639f53c80
Rishi <rishiraj.sahoo@niser.ac.in>
2025-07-31 17:30:18 +0530

    fixed all the conflictions, both servers are running

--- code/backend/pothole_tracker/settings.py
+++ code/backend/pothole_tracker/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # Enter Your secret_key to enable!
-SECRET_KEY = '#######'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
